05381031242d0f682b870527f57f4e5c795e4f87
konleyL <154012558+konleyL@users.noreply.github.com>
2024-03-18 21:55:55 +0000

    added animations

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ const header = document.querySelector('header');
 document.addEventListener('click', (e) => {
     target = e.target;
     target.style.color = 'var(--green)';
+   
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    myName.classList.add('slide-up');
+    role.classList.add('slide-down');
 })
