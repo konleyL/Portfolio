dcf452838da6e820e94cb28882500ffddc7bcd17
konleyL <154012558+konleyL@users.noreply.github.com>
2024-03-19 02:13:37 +0000

    uploaded better quality me.png adjusted stylings to incorporate grid added animations for the landing page

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const myName = document.querySelector('#name');
 const role = document.querySelector('#role');
 const header = document.querySelector('header');
+const arrow = document.querySelector('#arrow')
 
 document.addEventListener('click', (e) => {
     target = e.target;
@@ -11,4 +12,5 @@ document.addEventListener('click', (e) => {
 document.addEventListener('DOMContentLoaded', () => {
     myName.classList.add('slide-up');
     role.classList.add('slide-down');
+    arrow.classList.add('slide-down-arrow');
 })
